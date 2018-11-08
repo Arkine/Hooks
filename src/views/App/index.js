7eb823826d94e9ba1a62cfb41319662ed684e15f
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2018-11-08 11:32:53 -0700

    added theme provider

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Router, Switch, Route} from 'react-router-dom'
+import {ThemeProvider} from 'styled-components'
 
 import history from '../../services/history'
 import AsyncComponent from '../../components/AsyncComponent';
@@ -7,6 +8,9 @@ import AsyncComponent from '../../components/AsyncComponent';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Loading from '../../components/Loading'
+
+import {App} from './styled';
+import theme from '../../services/theme'
 
 // import Home from '../Home'
 const AsyncHome = AsyncComponent({
@@ -16,14 +20,16 @@ const AsyncHome = AsyncComponent({
 
 export default function() {
 	return (
-		<Router history={history}>
-			<main>
-				<Header />
-				<Switch>
-					<Route exact path='/' component={AsyncHome} />
-				</Switch>
-				<Footer />
-			</main>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router history={history}>
+				<App>
+					<Header />
+					<Switch>
+						<Route exact path='/' component={AsyncHome} />
+					</Switch>
+					<Footer />
+				</App>
+			</Router>
+		</ThemeProvider>
 	);
 }
