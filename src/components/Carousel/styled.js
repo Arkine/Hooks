f82333227642ebc8072bb4c02254fada3d204b19
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2018-11-09 15:33:10 -0700

    added font awesome icons

--- src/components/Carousel/styled.js
+++ src/components/Carousel/styled.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faArrowLeft, faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
 export const Carousel = styled.div`
 	position: absolute;
@@ -28,16 +30,24 @@ Carousel.Controls = styled.div`
 	z-index: 30;
 `
 
-Carousel.Control = styled.div`
+Carousel.Control = styled(FontAwesomeIcon)`
 	position: absolute;
 	top: 50%;
 	z-index: 30;
+
+	&:hover {
+		cursor: pointer;
+	}
 `
 
-Carousel.Control_Left = styled(Carousel.Control)`
+Carousel.Control_Left = styled(Carousel.Control).attrs({
+	icon: faArrowLeft
+})`
 	left: 1rem;
 `
 
-Carousel.Control_Right = styled(Carousel.Control)`
+Carousel.Control_Right = styled(Carousel.Control).attrs({
+	icon: faArrowRight
+})`
 	right: 1rem;
 `
