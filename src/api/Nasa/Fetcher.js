af9b7a7cca35ce22f6c15cb3becc143562baab30
arkine <slyngstad55@gmail.com>
2018-11-10 14:28:05 -0700

    moved api config out

--- src/api/Nasa/Fetcher.js
+++ src/api/Nasa/Fetcher.js
@@ -1,4 +1,4 @@
-import config from './config'
+import config from '../../config'
 
 class Fecther {
 	constructor(url = config.url, fetchOptions) {
---
+++ src/config.sample.js
@@ -0,0 +1,4 @@
+export default {
+	key: 'key here',
+	url: 'url here'
+}
